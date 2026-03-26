319e165cf195d771f80f2e963d86826af785ba6d
Ziyaowei1217 <ziyaowei1217@gmail.com>
2026-03-26 18:01:59 -0400

    fix: run black code linter on code

--- 01-llm_call.py
+++ 01-llm_call.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 client = genai.Client()
 
+
 def generate_prompt():
     return f"""write the python code to calculate
 a loan payment with the following inputs: interest,
@@ -13,9 +14,9 @@ term, present value. return code only wrapped in a Markdown
 code block (triple backticks). Do not add any extra text or 
 explanation outside the code block."""
 
+
 response = client.models.generate_content(
-    model = "gemini-2.5-flash",
-    contents     = generate_prompt()
+    model="gemini-2.5-flash", contents=generate_prompt()
 )
 
 match = re.search(r"```.*?([\w\W]*?)```", response.text, re.DOTALL)
@@ -26,19 +27,3 @@ print(code_content)
 with open("loan_calc.py", "w") as f:
     f.write(code_content)
     f.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
